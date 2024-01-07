a539f6647d79db6bba80073c9ec09f9b84513dd4
Johan Flint <johan.flint@gmail.com>
2024-01-07 15:08:03 +0100

    Run all observers on separate schedulers and subscribe the store

--- src/main/kotlin/com/larastudios/chambrier/app/AppListener.kt
+++ src/main/kotlin/com/larastudios/chambrier/app/AppListener.kt
@@ -1,16 +1,30 @@
 package com.larastudios.chambrier.app
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.context.ApplicationListener
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+import reactor.core.scheduler.Schedulers
 
 @Component
-class AppListener(val observers: List<Observer>) : ApplicationListener<ContextRefreshedEvent> {
+class AppListener(val observers: List<Observer>, val store: Store) : ApplicationListener<ContextRefreshedEvent> {
     override fun onApplicationEvent(event: ContextRefreshedEvent) {
-        for (observer in observers) {
-            observer.observe()
-                .log()
-                .subscribe()
-        }
+        logger.info { "Found ${observers.size} observers..." }
+
+        val events = Flux.merge(
+            observers.map {
+                it.observe().publishOn(Schedulers.parallel())
+            })
+
+        logger.info { "Store subscribing to event stream..." }
+        store.subscribe(events)
+        logger.info { "Store subscribing to event stream... OK" }
+
+        store.state().log().subscribe()
+    }
+
+    companion object {
+        private val logger = KotlinLogging.logger {}
     }
 }
