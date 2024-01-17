bb895376b8abb7f79b7c4090f96b16bb709860da
Johan Flint <johan.flint@gmail.com>
2024-01-17 18:12:52 +0100

    Define events to change property values

--- src/main/kotlin/com/larastudios/chambrier/app/domain/Event.kt
+++ src/main/kotlin/com/larastudios/chambrier/app/domain/Event.kt
@@ -3,3 +3,28 @@ package com.larastudios.chambrier.app.domain
 interface Event
 
 data class DiscoveredDevices(val devices: List<Device>) : Event
+
+data class BooleanPropertyChanged(
+    val deviceId: String,
+    val propertyId: String,
+    val value: Boolean,
+) : Event
+
+data class NumberPropertyChanged(
+    val deviceId: String,
+    val propertyId: String,
+    val value: Number,
+) : Event
+
+data class ColorPropertyChanged(
+    val deviceId: String,
+    val propertyId: String,
+    val xy: CartesianCoordinate,
+    val gamut: Gamut?,
+) : Event
+
+data class EnumPropertyChanged<T : Enum<T>>(
+    val deviceId: String,
+    val propertyId: String,
+    val value: T,
+) : Event
