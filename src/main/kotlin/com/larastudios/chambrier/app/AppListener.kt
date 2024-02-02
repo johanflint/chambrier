bd56e274a47b9e40bc3781da94c105997565e328
Johan Flint <johan.flint@gmail.com>
2024-02-02 16:48:09 +0100

    Listen to changes to the store state in a coroutine

--- src/main/kotlin/com/larastudios/chambrier/app/AppListener.kt
+++ src/main/kotlin/com/larastudios/chambrier/app/AppListener.kt
@@ -5,7 +5,10 @@ import com.larastudios.chambrier.app.domain.FlowContext
 import com.larastudios.chambrier.app.flowEngine.ControlDeviceAction
 import com.larastudios.chambrier.app.flowEngine.FlowEngine
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
+import kotlinx.coroutines.reactive.asFlow
+import org.springframework.context.ApplicationListener
+import org.springframework.context.event.ContextClosedEvent
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
@@ -24,22 +27,24 @@ class AppListener(
     suspend fun handleContextRefreshEvent(event: ContextRefreshedEvent): Unit = coroutineScope {
         val flows = flowLoader.load()
 
-        store.state()
-            .doOnNext { state ->
-                val commands = flows.map { flowEngine.execute(it, FlowContext(state)) }
-                    .map {
-                        getCommandMap(it.scope)
-                    }
-                    .fold(mapOf(), ::mergeCommandMaps)
-                    .map { (deviceId, propertyMap) ->
-                        val device = state.devices[deviceId] ?: throw IllegalArgumentException("State contains no device with id '$deviceId'")
-                        ControlDeviceCommand(device, propertyMap)
-                    }
+        launch(CoroutineName("storeListener")) {
+            store.state().asFlow()
+                .collect { state ->
+                    val commands = flows.map { flowEngine.execute(it, FlowContext(state)) }
+                        .map {
+                            getCommandMap(it.scope)
+                        }
+                        .fold(mapOf(), ::mergeCommandMaps)
+                        .map { (deviceId, propertyMap) ->
+                            val device = state.devices[deviceId] ?: throw IllegalArgumentException("State contains no device with id '$deviceId'")
+                            ControlDeviceCommand(device, propertyMap)
+                        }
 
-                controllers.forEach {
-                    it.send(commands)
+                    controllers.forEach {
+                        it.send(commands)
+                    }
                 }
-            }.subscribe()
+        }
 
         val events = Flux.merge(
             observers.map {
