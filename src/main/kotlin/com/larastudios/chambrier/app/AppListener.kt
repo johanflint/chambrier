5d4593643cdf239af8ec774a82f43b5a65b12129
Johan Flint <johan.flint@gmail.com>
2024-02-22 18:26:29 +0100

    Run every flow in a separate coroutine
    Otherwise when the wait action will suspend, all flows that have to run afterwards are also waiting as it's all the same coroutine.

--- src/main/kotlin/com/larastudios/chambrier/app/AppListener.kt
+++ src/main/kotlin/com/larastudios/chambrier/app/AppListener.kt
@@ -6,12 +6,10 @@ import com.larastudios.chambrier.app.domain.FlowContext
 import com.larastudios.chambrier.app.flowEngine.ControlDeviceAction
 import com.larastudios.chambrier.app.flowEngine.FlowEngine
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.merge
-import kotlinx.coroutines.launch
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
@@ -31,9 +29,11 @@ class AppListener(
 
         launch(CoroutineName("commandChannel")) {
             commandChannel.consumeEach { commands ->
-                logger.debug { "[commandChannel] Received ${commands.joinToString(", ")}" }
-                controllers.forEach {
-                    it.send(commands)
+                if (commands.isNotEmpty()) {
+                    logger.debug { "[commandChannel] Received ${commands.size} commands: ${commands.joinToString(", ")}" }
+                    controllers.forEach {
+                        it.send(commands)
+                    }
                 }
             }
         }
@@ -41,20 +41,21 @@ class AppListener(
         launch(CoroutineName("storeListener")) {
             store.state()
                 .collect { state ->
-                    logger.info { "[storeListener] $state" }
-                    flows.map { flowEngine.execute(it, FlowContext(state, commandChannel)) }
-                        .map {
-                            getCommandMap(it.scope)
+                    logger.info { "[storeListener] New state: $state" }
+                    val commandMaps = flows.map { flow ->
+                        async {
+                            val report = flowEngine.execute(flow, FlowContext(state, commandChannel))
+                            getCommandMap(report.scope)
                         }
-                        .fold(mapOf(), ::mergeCommandMaps)
+                    }.awaitAll()
+
+                    val commands = commandMaps.fold(mapOf(), ::mergeCommandMaps)
                         .map { (deviceId, propertyMap) ->
                             val device = state.devices[deviceId] ?: throw IllegalArgumentException("State contains no device with id '$deviceId'")
                             ControlDeviceCommand(device, propertyMap)
                         }
 
-                        if (commands.isNotEmpty()) {
-                            commandChannel.send(commands)
-                        }
+                    commandChannel.send(commands)
                 }
         }
 
